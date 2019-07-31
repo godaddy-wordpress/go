4591b491e323958620218e387439b86249ce769d
Rich Tabor <hi@richtabor.com>
2019-07-31 17:46:11 -0400

    Disable editor styles temporarily

--- includes/core.php
+++ includes/core.php
@@ -75,7 +75,7 @@ function theme_setup() {
 	add_theme_support( 'disable-custom-font-sizes' );
 	add_theme_support( 'responsive-embeds' );
 	add_theme_support( 'align-wide' );
-	add_theme_support( 'editor-styles' );
+	// add_theme_support( 'editor-styles' );
 	add_theme_support( 'wp-block-styles' );
 	add_theme_support( 'woocommerce' );
 
