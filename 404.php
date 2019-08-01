c2c1bbf3ba0a6de4d493cf0ab23a4f15176b2668
Evan Herman <evan.m.herman@gmail.com>
2019-08-01 15:20:48 -0400

    Update 404. Remove autocomplete from search input.

--- 404.php
+++ 404.php
@@ -8,7 +8,15 @@
 get_header(); ?>
 
 	<div id="content" class="content-area">
-		<?php get_template_part( 'partials/content', 'none' ); ?>
+		<section class="error-404 not-found center-align">
+			<header class="entry-header">
+				<h1 class="post__title"><?php esc_html_e( '404', 'maverick' ); ?></h1>
+				<h2 class="h2"><?php esc_html_e( 'This isn’t what you’re looking for.', 'maverick' ); ?></h2>
+			</header>
+			<div class="page-content container--sml">
+				<?php get_search_form(); ?>
+			</div>
+		</section>
 	</div>
 
 <?php
