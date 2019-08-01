d43ea6a38da1267affc8da889dc2de278c1abfda
Evan Herman <evan.m.herman@gmail.com>
2019-08-01 15:27:24 -0400

    Update 404 spacing

--- 404.php
+++ 404.php
@@ -9,17 +9,17 @@ get_header();
 
 ?>
 
-	<div id="content" class="content-area">
-		<section class="error-404 not-found center-align">
-			<header class="entry-header">
-				<h1 class="post__title"><?php esc_html_e( '404', 'maverick' ); ?></h1>
-				<h2 class="h2"><?php esc_html_e( 'This isn’t what you’re looking for.', 'maverick' ); ?></h2>
-			</header>
-			<div class="page-content container--sml">
-				<?php get_search_form(); ?>
-			</div>
-		</section>
-	</div>
+<div id="content" class="content-area">
+	<section class="error-404 not-found center-align">
+		<header class="entry-header">
+			<h1 class="post__title"><?php esc_html_e( '404', 'maverick' ); ?></h1>
+			<h2 class="h2"><?php esc_html_e( 'This isn’t what you’re looking for.', 'maverick' ); ?></h2>
+		</header>
+		<div class="page-content container--sml">
+			<?php get_search_form(); ?>
+		</div>
+	</section>
+</div>
 
 <?php
 
