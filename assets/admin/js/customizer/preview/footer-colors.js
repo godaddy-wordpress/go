4a4f24e9c6fcab0d0e533d9f5c39dc30eb3b341a
Rich Tabor <hi@richtabor.com>
2019-08-02 10:37:27 -0400

    Fix/customizer color live preview (#119)
    * Remove unnecessary c-primary-menu class
    
    * Fix #117

--- assets/admin/js/customizer/preview/footer-colors.js
+++ assets/admin/js/customizer/preview/footer-colors.js
@@ -12,11 +12,13 @@ export default () => {
 	wp.customize( 'footer_background_color', ( value ) => {
 		value.bind( ( to ) => {
 			const hsl = hexToHSL( to );
+			const setTo = to ? `${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%` : undefined;
+			document.querySelector( ':root' ).style.setProperty( '--theme-footer--bg', setTo );
+
+			// Add class if a background color is applied.
 			if ( to ) {
-				document.querySelector( ':root' ).style.setProperty( '--theme-footer--bg', `${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%` );
 				$( '.site-footer' ).addClass( 'has-background' );
 			} else {
-				document.querySelector( ':root' ).style.setProperty( '--theme-footer--bg', undefined );
 				$( '.site-footer' ).removeClass( 'has-background' );
 			}
 		} );
@@ -24,21 +26,25 @@ export default () => {
 
 	wp.customize( 'footer_social_color', ( value ) => {
 		value.bind( ( to ) => {
-			$( '.social-icons__icon svg' ).css( 'fill', to );
+			const hsl = hexToHSL( to );
+			const setTo = to ? `${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%` : undefined;
+			document.querySelector( ':root' ).style.setProperty( '--theme-social--color', setTo );
 		} );
 	} );
 
 	wp.customize( 'footer_text_color', ( value ) => {
 		value.bind( ( to ) => {
 			const hsl = hexToHSL( to );
-			document.querySelector( ':root' ).style.setProperty( '--theme-footer--color', `${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%` );
+			const setTo = to ? `${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%` : undefined;
+			document.querySelector( ':root' ).style.setProperty( '--theme-footer--color', setTo );
 		} );
 	} );
 
 	wp.customize( 'footer_heading_color', ( value ) => {
 		value.bind( ( to ) => {
 			const hsl = hexToHSL( to );
-			document.querySelector( ':root' ).style.setProperty( '--theme-footer-heading--color', `${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%` );
+			const setTo = to ? `${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%` : null;
+			document.querySelector( ':root' ).style.setProperty( '--theme-footer-heading--color', setTo );
 		} );
 	} );
 };
--- assets/admin/js/customizer/preview/header-colors.js
+++ assets/admin/js/customizer/preview/header-colors.js
@@ -12,18 +12,19 @@ export default () => {
 	wp.customize( 'header_background_color', ( value ) => {
 		value.bind( ( to ) => {
 			const hsl = hexToHSL( to );
-			document.querySelector( ':root' ).style.setProperty( '--theme-header--bg', `${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%` );
+			const setTo = to ? `${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%` : undefined ;
+			document.querySelector( ':root' ).style.setProperty( '--theme-header--bg', setTo );
 		} );
 	} );
 
 	wp.customize( 'maverick_header_text_color_setting', ( value ) => {
 		value.bind( ( to ) => {
 			const hsl = hexToHSL( to );
-			document.querySelector( '.c-primary-menu' ).style.setProperty( '--theme-site-nav--color', `${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%` );
-			document.querySelector( '.site-branding' ).style.setProperty( '--theme-link-color', `${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%` );
-			document.querySelector( ':root' ).style.setProperty( '--theme-site-description--color', `${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%` );
-			document.querySelector( '.site-search__toggle' ).style.setProperty( '--theme-search-toggle--color', `${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%` );
-			document.querySelector( '.site-branding__title' ).style.setProperty( '--theme-site-title--color', `${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%` );
+			const setTo = to ? `${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%` : undefined ;
+			document.querySelector( ':root' ).style.setProperty( '--theme-site-nav--color', setTo );
+			document.querySelector( ':root' ).style.setProperty( '--theme-site-description--color', setTo );
+			document.querySelector( ':root' ).style.setProperty( '--theme-search-toggle--color', setTo );
+			document.querySelector( ':root' ).style.setProperty( '--theme-site-title--color', setTo );
 		} );
 	} );
 };
