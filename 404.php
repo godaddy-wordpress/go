ca9cc3b567b103a4cc80e17f9198170454b72267
Rich Tabor <hi@richtabor.com>
2019-08-02 13:08:40 -0400

    Small tweaks to 404

--- 404.php
+++ 404.php
@@ -9,16 +9,13 @@ get_header();
 
 ?>
 
-<div id="content" class="content-area">
-	<section class="error-404 not-found center-align">
-		<header class="entry-header">
-			<h1 class="post__title"><?php esc_html_e( '404', 'maverick' ); ?></h1>
-			<h2 class="h2"><?php esc_html_e( 'This isn’t what you’re looking for.', 'maverick' ); ?></h2>
-		</header>
-		<div class="page-content container--sml">
-			<?php get_search_form(); ?>
-		</div>
-	</section>
+<div class="error-404 not-found max-w-base m-auto">
+	<header class="entry-header">
+		<h1 class="page-title"><?php esc_html_e( 'That page can’t be found.', 'maverick' ); ?></h1>
+	</header>
+	<div class="page-content">
+		<?php get_search_form(); ?>
+	</div>
 </div>
 
 <?php
