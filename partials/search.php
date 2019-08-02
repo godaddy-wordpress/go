ab10e34c3f6970997615fa2535047ec360c219f2
Evan Herman <evanmherman@gmail.com>
2019-08-02 13:09:02 -0400

    Update 404 template (#113)
    * Update 404. Remove autocomplete from search input.
    
    * Update 404 spacing
    
    * Tweak styles
    
    *  Tweak 404.css
    
    * Add line height to 404 h2
    
    * Update heading leading
    
    * Update media.css
    
    * Small tweaks to 404

--- partials/search.php
+++ partials/search.php
@@ -11,7 +11,7 @@
 	<label for="search-form__label">
 		<span class="screen-reader-text"><?php echo esc_html_x( 'Search for:', 'label', 'maverick' ); ?></span>
 	</label>
-	<input itemprop="query-input" type="search" id="search-field" class="input input--search search-form__input" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'maverick' ); ?>" name="s" />
+	<input itemprop="query-input" type="search" id="search-field" class="input input--search search-form__input" autocomplete="off" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'maverick' ); ?>" name="s" />
 	<button type="submit" class="button search-input__button">
 		<span class="search-input__label"><?php echo esc_attr_x( 'Submit', 'submit button', 'maverick' ); ?></span>
 		<svg class="search-input__arrow-icon" width="30" height="28" viewBox="0 0 30 28" fill="inherit" xmlns="http://www.w3.org/2000/svg">
