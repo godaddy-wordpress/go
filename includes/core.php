478543d4b0c529379720630b5968ded521bc3524
Rich Tabor <hi@richtabor.com>
2019-08-07 16:06:52 -0400

    Remove icon_class from social_icons (#170)

--- includes/core.php
+++ includes/core.php
@@ -820,31 +820,26 @@ function get_available_social_icons() {
 		'facebook'  => [
 			'label'       => esc_html__( 'Facebook', 'maverick' ),
 			'icon'        => MAVERICK_PATH . '/assets/shared/images/social/facebook.svg',
-			'icon_class'  => '',
 			'placeholder' => 'https://facebook.com/user',
 		],
 		'twitter'   => [
 			'label'       => esc_html__( 'Twitter', 'maverick' ),
 			'icon'        => MAVERICK_PATH . '/assets/shared/images/social/twitter.svg',
-			'icon_class'  => '',
 			'placeholder' => 'https://twitter.com/user',
 		],
 		'instagram' => [
 			'label'       => esc_html__( 'Instagram', 'maverick' ),
 			'icon'        => MAVERICK_PATH . '/assets/shared/images/social/instagram.svg',
-			'icon_class'  => '',
 			'placeholder' => 'https://instagram.com/user',
 		],
 		'linkedin'  => [
 			'label'       => esc_html__( 'LinkedIn', 'maverick' ),
 			'icon'        => MAVERICK_PATH . '/assets/shared/images/social/linkedin.svg',
-			'icon_class'  => '',
 			'placeholder' => 'https://linkedin.com/in/user',
 		],
 		'pinterest' => [
 			'label'       => esc_html__( 'Pinterest', 'maverick' ),
 			'icon'        => MAVERICK_PATH . '/assets/shared/images/social/pinterest.svg',
-			'icon_class'  => '',
 			'placeholder' => 'https://pinterest.com/user',
 		],
 	];
