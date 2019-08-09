763b767aaf5e8444b9afa5b239437f6032d4c822
Rich Tabor <hi@richtabor.com>
2019-08-09 13:33:45 -0400

    Update header-1.php

--- partials/headers/header-1.php
+++ partials/headers/header-1.php
@@ -34,7 +34,7 @@
 					<span class="screen-reader-text"><?php esc_html_e( 'Search Toggle', 'maverick' ); ?></span>
 				</button>
 
-				<?php get_search_form( 'header' ); ?>
+				<?php get_search_form(); ?>
 
 			</nav>
 		<?php } else { ?>
