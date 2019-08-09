b231e0ffc77a732e9571b803d8fa8d0332767193
Rich Tabor <hi@richtabor.com>
2019-08-09 16:56:34 -0400

    Update footer-1.php

--- partials/footers/footer-1.php
+++ partials/footers/footer-1.php
@@ -5,8 +5,7 @@
  * @package Maverick
  */
 
-$footer_copy_text = Maverick\footer_copy_text();
-$has_background   = Maverick\has_footer_background();
+$has_background = Maverick\has_footer_background();
 ?>
 
 <footer id="colophon" class="site-footer site-footer--1 <?php echo esc_attr( $has_background ); ?>">
@@ -31,17 +30,7 @@ $has_background   = Maverick\has_footer_background();
 			<p class="u-informational"><a href="<?php echo esc_url( admin_url( 'nav-menus.php' ) ); ?>"><?php esc_html_e( 'Please assign a menu to the Footer Menu #1', 'maverick' ); ?></a></p>
 		<?php } ?>
 
-		<?php if ( ! empty( $footer_copy_text ) ) : ?>
-			<p class="site-info text-sm mb-0">
-				<?php echo esc_html( $footer_copy_text ); ?>
-
-				<?php
-				if ( function_exists( 'the_privacy_policy_link' ) ) {
-					the_privacy_policy_link( '' );
-				}
-				?>
-			</p>
-		<?php endif; ?>
+		<?php Maverick\copyright( [ 'class' => 'site-info text-sm mb-0' ] ); ?>
 
 	</div>
 
