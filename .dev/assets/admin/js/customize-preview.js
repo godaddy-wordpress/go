1074abef637d329cb2e0bf371dde2c3c8298ac37
Rich Tabor <hi@richtabor.com>
2019-08-09 16:56:13 -0400

    Remove blurb, simplify copyright into copyright()

--- .dev/assets/admin/js/customize-preview.js
+++ .dev/assets/admin/js/customize-preview.js
@@ -1,6 +1,6 @@
 import DesignStylePreview from './customizer/preview/design-style';
-import HeaderColorsPreview from './customizer/preview/header-colors';
-import FooterColorsPreview from './customizer/preview/footer-colors';
+import HeaderColorsPreview from './customizer/preview/header';
+import FooterColorsPreview from './customizer/preview/footer';
 import ColorSchemePreview from './customizer/preview/color-schemes';
 import LogoSizingPreview from './customizer/preview/logo-sizing';
 
