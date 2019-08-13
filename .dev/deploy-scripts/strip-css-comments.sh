4aeca32fceaa782f895190b0e93698f2c71e7ad5
Evan Herman <evan.m.herman@gmail.com>
2019-08-13 17:15:43 -0400

    Preserve /*! comments

--- .dev/deploy-scripts/strip-css-comments.sh
+++ .dev/deploy-scripts/strip-css-comments.sh
@@ -3,7 +3,6 @@
 for f in $(find ./dist/css -type f -name "*.css")
 do
 	echo "Cleaning $f file of comments..."
-	CONTENT=$(npx strip-css-comments --no-preserve $f)
-	echo "$CONTENT" > $f
+	echo "$(npx strip-css-comments $f)" > $f
 	npx perfectionist $f $f
 done
