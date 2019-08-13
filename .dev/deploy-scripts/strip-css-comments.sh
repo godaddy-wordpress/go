c3796ae677ae559219c9be41294c078753533d1e
Evan Herman <evan.m.herman@gmail.com>
2019-08-13 09:33:42 -0400

    Updates

--- .dev/deploy-scripts/strip-css-comments.sh
+++ .dev/deploy-scripts/strip-css-comments.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ "$NODE_ENV" = 'production' ];
+then
+	return;
+fi
+
+echo "NODE ENVIRONMENT:"
+echo $NODE_ENV
+
 for f in $(find ./dist/css -type f -name "*.css")
 do
 	echo "Cleaning $f file of comments..."
