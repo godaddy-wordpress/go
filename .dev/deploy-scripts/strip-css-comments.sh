1a43b30594fed933197f06db9ea7820db3560a25
Evan Herman <evan.m.herman@gmail.com>
2019-08-13 09:48:06 -0400

    Cleanup excess code

--- .dev/deploy-scripts/strip-css-comments.sh
+++ .dev/deploy-scripts/strip-css-comments.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-if [ "$NODE_ENV" = 'production' ];
-then
-	return;
-fi
-
-echo "NODE ENVIRONMENT:"
-echo $NODE_ENV
-
 for f in $(find ./dist/css -type f -name "*.css")
 do
 	echo "Cleaning $f file of comments..."
-	#!/bin/bash
 	CONTENT=$(npx strip-css-comments --no-preserve $f)
 	echo "$CONTENT" > $f
 	npx perfectionist $f $f
