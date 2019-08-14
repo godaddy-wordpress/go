079cf9c3762051a99ae96aecea28f8550040d4b2
Evan Herman <evan.m.herman@gmail.com>
2019-08-14 09:35:46 -0400

    Update deployment pipeline, setup grunt version, setup npm version major|minor|patch

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,38 +10,6 @@ module.exports = function( grunt ) {
 
 		pkg: pkg,
 
-		clean: {
-			options: {
-				force: true
-			},
-			docs: [ '.dev/docs/sphinx/src/documentation/' ]
-		},
-
-		copy: {
-			docs_html: {
-				expand: true,
-				cwd: '.dev/docs/sphinx/src/documentation/',
-				src: [ '**/*' ],
-				dest: '.dev/docs/build/html/documentation/'
-			},
-			docs_landing: {
-				expand: true,
-				cwd: '.dev/docs/sphinx/src/build/html/',
-				src: [ '**/*.html' ],
-				dest: '.dev/docs/build/html/'
-			},
-			readme: {
-				expand: true,
-				dot: true,
-				cwd: '.',
-				dest: '.dev/docs/sphinx/src/',
-				src: [ 'readme.md' ],
-				rename: function( dest, src ) {
-					return dest + src.replace( 'readme', 'intro' );
-				}
-			}
-		},
-
 		devUpdate: {
 			packages: {
 				options: {
@@ -58,73 +26,7 @@ module.exports = function( grunt ) {
 			}
 		},
 
-		imagemin: {
-			options: {
-				optimizationLevel: 3
-			},
-			assets: {
-				expand: true,
-				cwd: 'assets/images/',
-				src: [ '**/*.{gif,jpeg,jpg,png,svg}' ],
-				dest: 'assets/images/'
-			},
-			screenshot: {
-				files: {
-					'screenshot.png': 'screenshot.png'
-				}
-			}
-		},
-
 		replace: {
-			charset: {
-				overwrite: true,
-				replacements: [
-					{
-						from: /^@charset "UTF-8";\n/,
-						to: ''
-					}
-				],
-				src: [ 'style*.css' ]
-			},
-			docs: {
-				overwrite: true,
-				replacements: [
-					{
-						from: /Primer Theme v[\w.+-]+/m,
-						to: 'Primer Theme v' + pkg.version
-					}
-				],
-				src: [
-					'.dev/docs/apigen/godaddy/**/*.latte',
-					'.dev/docs/sphinx/godaddy/**/*.html'
-				]
-			},
-			docs_version: {
-				overwrite: true,
-				replacements: [
-					{
-						from: /activeVersion: \'[\w.+-]+\'/m,
-						to: 'activeVersion: \'' + pkg.version + '\''
-					}
-				],
-				src: [
-					'.dev/docs/apigen/godaddy/config.neon'
-				]
-			},
-			intro: {
-				overwrite: true,
-				replacements: [
-					{
-						from: /^\[!\[(.|\r|\n)*## Description ##/m, // Badges cause errors in the sphinx build process
-						to: '## Description ##'
-					},
-					{
-						from: /  $/gm,
-						to: '<br />'
-					}
-				],
-				src: [ '.dev/docs/sphinx/src/intro.md' ]
-			},
 			php: {
 				overwrite: true,
 				replacements: [
@@ -141,23 +43,13 @@ module.exports = function( grunt ) {
 						to: '<%= pkg.version %>'
 					},
 					{
-						from: /'PRIMER_VERSION',(\s*)'[\w.+-]+'/,
-						to: "'PRIMER_VERSION',$1'<%= pkg.version %>'"
+						from: /'MAVERICK_VERSION',(\s*)'[\w.+-]+'/,
+						to: "'MAVERICK_VERSION',$1'<%= pkg.version %>'"
 					}
 				],
-				src: [ '*.php', 'inc/**/*.php', 'templates/**/*.php' ]
+				src: [ '*.php', '**/*.php' ]
 			},
-			readme: {
-				overwrite: true,
-				replacements: [
-					{
-						from: /Stable tag:(\s*)[\w.+-]+/,
-						to: 'Stable tag:$1<%= pkg.version %>'
-					}
-				],
-				src: [ 'readme.txt' ]
-			},
-			sass: {
+			style_css: {
 				overwrite: true,
 				replacements: [
 					{
@@ -165,7 +57,7 @@ module.exports = function( grunt ) {
 						to: 'Version:$1<%= pkg.version %>'
 					}
 				],
-				src: [ '.dev/sass/**/*.scss' ]
+				src: [ 'style.css' ]
 			}
 		}
 
@@ -173,8 +65,8 @@ module.exports = function( grunt ) {
 
 	require( 'matchdep' ).filterDev( 'grunt-*' ).forEach( grunt.loadNpmTasks );
 
-	grunt.registerTask( 'default', [ 'replace:charset', 'imagemin' ] );
+	grunt.registerTask( 'default', [ 'replace' ] );
+	grunt.registerTask( 'version', [ 'replace' ] );
 	grunt.registerTask( 'check',   [ 'devUpdate' ] );
-	grunt.registerTask( 'version', [ 'replace', 'default', 'clean' ] );
 
 };
