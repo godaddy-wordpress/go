4df52883ed88b10052cf7510bbb09d7b7d23df09
JR Tashjian <jrtashjian@gmail.com>
2019-08-15 15:42:36 -0400

    Customizer edit shortcut added to all empty navigation menus. (#210)
    * Remove custom styling for u-informational in header.
    
    * Add a fallback to every wp_nav_menu call.
    
    * Update how wp_nav_menu is called with the new fallback
    
    * Update shared-style.css
    
    * phpcs

--- partials/headers/header-5.php
+++ partials/headers/header-5.php
@@ -12,26 +12,21 @@ $has_background = Maverick\has_header_background();
 
 	<div class="site-header__inner flex items-center justify-center max-w-wide m-auto relative">
 
-		<?php if ( has_nav_menu( 'primary' ) ) { ?>
+		<nav id="js-primary-menu" class="site-navigation c-site-navigation" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
 
-			<nav id="js-primary-menu" class="site-navigation c-site-navigation" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
+			<?php Maverick\navigation_toggle(); ?>
 
-				<?php Maverick\navigation_toggle(); ?>
+			<?php
+				wp_nav_menu(
+					[
+						'theme_location' => 'primary',
+						'menu_class'     => 'primary-menu list-reset',
+						'container'      => false,
+					]
+				);
+			?>
 
-				<?php
-					wp_nav_menu(
-						[
-							'theme_location' => 'primary',
-							'menu_class'     => 'primary-menu list-reset',
-							'container'      => false,
-						]
-					);
-				?>
-			</nav><!-- .site-navigation -->
-
-		<?php } else { ?>
-			<p class="u-informational"><a href="<?php echo esc_url( admin_url( 'nav-menus.php' ) ); ?>"><?php esc_html_e( 'Please assign a Primary menu to the header', 'maverick' ); ?></a></p>
-		<?php } ?>
+		</nav><!-- .site-navigation -->
 
 		<?php Maverick\display_site_branding(); ?>
 
