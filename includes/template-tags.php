0c94ec68482d4a0fb6888c0007177af23a9c394d
Evan Herman <evan.m.herman@gmail.com>
2019-08-16 10:28:32 -0400

    Remove parenthasis around include_once

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -397,7 +397,7 @@ function social_icons( $args = [] ) {
 			<?php if ( ! empty( $social_icon['url'] ) || is_customize_preview() ) : ?>
 				<li class="<?php echo esc_attr( sprintf( $args['li_class'], $key ) ) . esc_attr( $visibility ); ?>">
 					<a class="social-icons__icon" href="<?php echo esc_url( $social_icon['url'] ); ?>" aria-label="<?php echo esc_attr( $social_icon['label'] ); ?>" rel="noopener noreferrer">
-						<?php include_once( $social_icon['icon'] ); ?>
+						<?php include_once $social_icon['icon']; ?>
 					</a>
 				</li>
 			<?php endif; ?>
@@ -489,6 +489,6 @@ function load_inline_svg( $filename ) {
 
 	}
 
-	include_once( MAVERICK_PATH . $svg_path . $filename );
+	include_once MAVERICK_PATH . $svg_path . $filename;
 
 }
