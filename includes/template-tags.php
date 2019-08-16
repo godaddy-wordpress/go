41d03f3fbfcb853e4f02b5f9e9c0f54a0d4b3d0f
Evan Herman <evan.m.herman@gmail.com>
2019-08-16 09:45:24 -0400

    Remove notice at top of template-tags.php

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -5,11 +5,6 @@
  * This file is for custom template tags only and it should not contain
  * functions that will be used for filtering or adding an action.
  *
- * All functions should be prefixed with Godaddy in order to prevent
- * pollution of the global namespace and potential conflicts with functions
- * from plugins.
- * Example: `tenup_function()`
- *
  * @package Maverick\Template_Tags
  */
 
