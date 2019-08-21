933e0a0b60d71bebe71c9902a8b5753eac9fea9c
Rich Tabor <hi@richtabor.com>
2019-08-21 18:27:00 -0400

    Add spaces

--- 404.php
+++ 404.php
@@ -10,10 +10,13 @@ get_header();
 ?>
 
 <div class="error-404 not-found max-w-base w-full m-auto p-x">
+
 	<?php Maverick\page_title(); ?>
+
 	<div class="page-content">
 		<?php get_search_form(); ?>
 	</div>
+
 </div>
 
 <?php
