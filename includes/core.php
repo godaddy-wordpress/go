b72e853c2697c06b1ed90a69ccf5422b246a18bc
Evan Herman <evan.m.herman@gmail.com>
2019-08-29 15:18:45 -0400

    Refactor to is_singular()

--- includes/core.php
+++ includes/core.php
@@ -498,7 +498,7 @@ function body_classes( $classes ) {
 	}
 
 	// Add class when a page or post has comments.
-	if ( ( is_single() || is_page() ) && comments_open() ) {
+	if ( is_singular() && comments_open() ) {
 		$classes[] = 'has-comments-open';
 	}
 
