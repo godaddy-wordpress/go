8fd4fb5668b485acc6d967a4c284e6f81e65410d
Evan Herman <evan.m.herman@gmail.com>
2019-08-30 13:54:16 -0400

    Refactor page_title

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -279,9 +279,8 @@ function copyright( $args = [] ) {
 function page_title() {
 
 	if (
-		! is_customize_preview() ||
-		is_front_page() &&
-		( ! get_theme_mod( 'page_titles', true ) && ! is_404() && ! is_search() && ! is_archive() )
+		! is_customize_preview() || is_front_page() &&
+		( is_front_page() || ( ! get_theme_mod( 'page_titles', true ) && ! is_404() && ! is_search() && ! is_archive() ) )
 	) {
 
 		return;
