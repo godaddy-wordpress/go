185fd40d92d5c84706b639d00d01fbe051254d97
Evan Herman <evanmherman@gmail.com>
2019-09-06 18:41:20 -0400

    Cross fade customizer when design styles change (#344)

--- .dev/assets/admin/js/customize/preview/design-style.js
+++ .dev/assets/admin/js/customize/preview/design-style.js
@@ -4,12 +4,22 @@ export default () => {
 	wp.customize( 'design_style', ( value ) => {
 		value.bind( ( to ) => {
 
+			$( '#customize-preview' ).addClass( 'is-loading' );
+
 			if (
 				'undefined' !== typeof MaverickPreviewData['design_styles'] &&
 				'undefined' !== MaverickPreviewData['design_styles'][ to ]
 			) {
-				const designStyle = MaverickPreviewData['design_styles'][ to ];
-				$( 'link[id*="design-style"]' ).attr( 'href', designStyle['url'] );
+
+				setTimeout( function() {
+					// wp.customize.previewer.refresh();
+					const designStyle = MaverickPreviewData['design_styles'][ to ];
+					$( 'link[id*="design-style"]' ).attr( 'href', designStyle['url'] );
+
+					setTimeout( function() {
+						$( '#customize-preview' ).removeClass( 'is-loading' );
+					}, 500 );
+				}, 500 ); // match the .02s transition time from core
 			}
 		} );
 	} );
