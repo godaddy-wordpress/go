3357dd0bde12ce05c2b373e319d7c30ae3a299d1
Rich Tabor <hi@richtabor.com>
2019-09-06 20:37:04 -0400

    Tweak index

--- partials/content.php
+++ partials/content.php
@@ -11,7 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<?php if ( has_post_thumbnail() ) : ?>
+	<?php if ( is_singular() && has_post_thumbnail() ) : ?>
 		<div class="post__thumbnail">
 			<?php the_post_thumbnail(); ?>
 		</div>
