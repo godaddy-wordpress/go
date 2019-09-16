789616afdcae77bf5ae61dd7d9dbc993cecf5e2b
Rich Tabor <hi@richtabor.com>
2019-09-16 17:13:20 -0400

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * The template for displaying Comments
+ * The template for displaying comments.
  *
  * The area of the page that contains both current comments
  * and the comment form. The actual display of comments is
  * handled by a callback to go_comment() which is
  * located in the functions.php file.
  *
- * @package go
+ * @package Go
  */
 
 /*
