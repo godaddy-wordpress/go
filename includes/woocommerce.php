cc56fca5408996b966efdc082434aa9c06463351
Evan Herman <evanmherman@gmail.com>
2019-10-19 14:31:04 -0400

    Introduce breadcrumbs and post navigation in single product templates (#385)
    * Add a header to the single product page
    
    * Tweak divider in post navigation
    
    * Update style-shared.css
    
    * Update single-product.css
    
    * Create close.svg
    
    * Tweaks

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -30,6 +30,22 @@ function setup() {
 
 	add_action( 'woocommerce_cart_is_empty', $n( 'empty_cart_message' ), 10 );
 
+	add_action( 'woocommerce_before_shop_loop', $n( 'sorting_wrapper' ), 9 );
+
+	add_action( 'woocommerce_after_shop_loop', $n( 'sorting_wrapper' ), 9 );
+
+	add_action( 'woocommerce_after_shop_loop', $n( 'sorting_wrapper_close' ), 31 );
+
+	add_action( 'woocommerce_before_shop_loop', $n( 'sorting_wrapper_close' ), 31 );
+
+	add_action( 'woocommerce_before_single_product', $n( 'single_product_header' ), 5 );
+
+	add_filter( 'woocommerce_breadcrumb_home_url', $n( 'breadcrumb_home_url' ) );
+
+	add_filter( 'woocommerce_product_description_heading', '__return_null' );
+
+	add_filter( 'woocommerce_product_additional_information_heading', '__return_null' );
+
 }
 
 /**
@@ -50,7 +66,7 @@ function empty_cart_message() {
 		</div>',
 		esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ),
 		wp_kses(
-			load_inline_svg( 'woocommerce/empty-cart.svg' ),
+			load_inline_svg( 'empty-cart.svg' ),
 			array_merge(
 				wp_kses_allowed_html( 'post' ),
 				[
@@ -78,38 +94,119 @@ function empty_cart_message() {
 
 }
 
-add_filter( 'woocommerce_product_description_heading', '__return_null' );
-add_filter( 'woocommerce_product_additional_information_heading', '__return_null' );
+/**
+ * Output single product header
+ *
+ * @return mixed Markup for the single product header
+ */
+function single_product_header() {
 
-if ( ! function_exists( 'go_sorting_wrapper' ) ) {
+	?>
 
-	/**
-	 * Sorting wrapper
-	 *
-	 * @return  void
-	 */
-	function go_sorting_wrapper() {
+	<div class="product-navigation-wrapper">
+		<?php
+			woocommerce_breadcrumb(
+				[
+					'delimiter' => '<span class="sep">&#47;</span>',
+					'home'      => woocommerce_page_title( false ),
+				]
+			);
+			single_product_pagination();
+			single_product_back_to_shop();
+		?>
+	</div>
 
-		echo '<div class="go-sorting">';
+	<?php
 
-	}
-}
-add_action( 'woocommerce_before_shop_loop', 'go_sorting_wrapper', 9 );
-add_action( 'woocommerce_after_shop_loop', 'go_sorting_wrapper', 9 );
-
-if ( ! function_exists( 'go_sorting_wrapper_close' ) ) {
-
-	/**
-	 * Sorting wrapper close
-	 *
-	 * @return  void
-	 */
-	function go_sorting_wrapper_close() {
-
-		echo '</div>';
-
-	}
 }
 
-add_action( 'woocommerce_after_shop_loop', 'go_sorting_wrapper_close', 31 );
-add_action( 'woocommerce_before_shop_loop', 'go_sorting_wrapper_close', 31 );
+/**
+ * Add post pagination to WooCommerce products.
+ */
+function single_product_pagination() {
+
+	ob_start();
+	load_inline_svg( 'arrow-left.svg' );
+	$arrow_left = ob_get_clean();
+
+	ob_start();
+	load_inline_svg( 'arrow-right.svg' );
+	$arrow_right = ob_get_clean();
+
+	the_post_navigation(
+		[
+			'prev_text' => '<span class="screen-reader-text">' . esc_html__( 'Previous Post: ', 'go' ) . ' %title</span><span class="nav-title-icon-wrapper">' . $arrow_left . '</span><span class="nav-title">' . esc_html__( 'Previous', 'go' ) . '</span>',
+			'next_text' => '<span class="screen-reader-text">' . esc_html__( 'Next Post:', 'go' ) . ' %title</span> <span class="nav-title">' . esc_html__( 'Next', 'go' ) . '<span class="nav-title-icon-wrapper">' . $arrow_right . '</span></span>',
+		]
+	);
+
+}
+
+/**
+ * Add the "back to shop" link to the single products, if on mobile.
+ */
+function single_product_back_to_shop() {
+
+	ob_start();
+	load_inline_svg( 'arrow-left.svg' );
+	$arrow_left = ob_get_clean();
+
+	/**
+	 * Filters the back to shop link URL.
+	 *
+	 * @since NEXT
+	 *
+	 * @param string URL to the WooCommerce shop page.
+	 */
+	$url = apply_filters( 'go_back_to_shop_url', get_permalink( wc_get_page_id( 'shop' ) ) );
+
+	/**
+	 * Filters the back to shop link text.
+	 *
+	 * @since NEXT
+	 *
+	 * @param string The text used in the back link.
+	 */
+	$text = apply_filters( 'go_back_to_shop_text', esc_html__( 'Back', 'go' ) );
+
+	printf(
+		'<a href="%s" class="back-to-shop">%s%s</a>',
+		esc_url( $url ),
+		$arrow_left, // @codingStandardsIgnoreLine
+		esc_html( $text )
+	);
+
+}
+
+/**
+ * Set the URL of the first link in the breadcrumbs to the shop page
+ *
+ * @return string URL to the shop page
+ */
+function breadcrumb_home_url() {
+
+	return get_permalink( wc_get_page_id( 'shop' ) );
+
+}
+
+/**
+ * Sorting wrapper
+ *
+ * @return  void
+ */
+function sorting_wrapper() {
+
+	echo '<div class="go-sorting">';
+
+}
+
+/**
+ * Sorting wrapper close
+ *
+ * @return  void
+ */
+function sorting_wrapper_close() {
+
+	echo '</div>';
+
+}
