bac65aedee089122fa1fc9dcd4cb9198f179c1ae
JR Tashjian <jrtashjian@gmail.com>
2019-10-25 15:56:45 -0400

    Add search-modal template partial

---
+++ partials/modal-search.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Displays the search icon and modal
+ *
+ * @package Go
+ */
+
+?>
+<div class="search-modal" data-modal-target-string=".search-modal" aria-expanded="false">
+
+	<div class="search-modal-inner">
+
+		<div class="section-inner">
+
+			<?php get_search_form(); ?>
+
+		</div><!-- .section-inner -->
+
+	</div><!-- .search-modal-inner -->
+
+</div><!-- .search-modal -->
