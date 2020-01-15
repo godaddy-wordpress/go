2ba2445ebcc4241c75e25b9176131751348cc77a
Evan Herman <evan.m.herman@gmail.com>
2020-01-15 17:45:24 -0500

    Rollback

--- .dev/tests/php/test-functions.php
+++ .dev/tests/php/test-functions.php
@@ -28,7 +28,7 @@ class Test_Functions extends WP_UnitTestCase {
 	 */
 	function testVersionDefined() {
 
-		$this->assertEquals( '1.2.2', GO_VERSION );
+		$this->assertEquals( '1.2.1', GO_VERSION );
 
 	}
 
