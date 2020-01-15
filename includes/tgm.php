0cf29390694729d12badfc38fd500d0657c9a2d7
Evan Herman <evan.m.herman@gmail.com>
2020-01-15 11:22:25 -0500

    Set is_automatic to false in includes/tgm.php

--- includes/tgm.php
+++ includes/tgm.php
@@ -53,7 +53,7 @@ function register_required_plugins() {
 		'has_notices'  => true,                    // Show admin notices or not.
 		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
 		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-		'is_automatic' => true,                    // Automatically activate plugins after installation or not.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
 		'message'      => '',                      // Message to output right before the plugins table.
 	);
 
