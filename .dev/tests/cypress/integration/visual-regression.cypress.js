3fcb2612af73e8ead8a32fe44319554625d45890
JR Tashjian <jrtashjian@gmail.com>
2020-04-14 16:26:35 -0400

    add our visual regression tests

---
+++ .dev/tests/cypress/integration/visual-regression.cypress.js
@@ -0,0 +1,61 @@
+
+function captureDocument( path = '' ) {
+    // Disable scroll-behavior as it prevents reliable full-page screenshots.
+    cy.get('html').invoke('css', 'scroll-behavior', 'auto');
+
+    [
+        { label: 'go-huge', width: 1440, height: 4000 },
+        { label: 'go-large', width: 960, height: 4000 },
+        { label: 'go-medium', width: 782, height: 4000 },
+        { label: 'go-small', width: 600, height: 4000 },
+
+    ].forEach( viewport => {
+        cy.viewport( viewport.width, viewport.height );
+        cy.matchImageSnapshot( `${path}/${viewport.label}` );
+    } );
+}
+
+function screenshotPathFromUrl( url ) {
+    const urlParts = new URL( url );
+    return [
+        urlParts.searchParams.get( 'lang' ),
+        urlParts.searchParams.get( 'template' ),
+        urlParts.searchParams.get( 'style' ),
+        urlParts.pathname.replace( '/v2/', '' ) || 'frontpage',
+    ].join( '/' );
+}
+
+describe( 'Template gallery regression testing', () => {
+    let templates;
+    let pages = [];
+
+    before( () => {
+        cy.request( 'https://wpnux.godaddy.com/v2/api/templates' )
+        .its( 'body' )
+        .should( 'exist' )
+        .then( ( list ) => {
+            templates = list;
+        } );
+    } );
+
+    it( 'Loads template home pages', () => {
+        templates.forEach( template => {
+            cy.visit( template.preview_url );
+            captureDocument( screenshotPathFromUrl( template.preview_url ) );
+
+            cy.get( '#header__navigation' ).then( $headerNavigation => {
+                [ ...$headerNavigation.find( '.menu-item a' ) ].forEach( $navLink => {
+                    pages.push( $navLink.href );
+                } );
+            } );
+        } );
+    } );
+
+    it( 'Loads additional pages', () => {
+        pages.forEach( page => {
+            cy.visit( page );
+            captureDocument( screenshotPathFromUrl( page ) );
+        } );
+    } );
+
+} );
