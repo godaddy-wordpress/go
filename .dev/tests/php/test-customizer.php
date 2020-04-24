3f09e40df6323bc275b792821e2e4f6f8f9ecc8c
AnthonyLedesma <anthonymledesma@gmail.com>
2020-04-24 10:42:43 -0700

    add tests for viewport_basis

--- .dev/tests/php/test-customizer.php
+++ .dev/tests/php/test-customizer.php
@@ -977,6 +977,29 @@ class Test_Customizer extends WP_UnitTestCase {
 	}
 
 	/**
+	 * Test the viewport_basis setting is registered
+	 */
+	function test_register_color_viewport_basis_setting() {
+
+		Go\Customizer\register_color_controls( $GLOBALS['wp_customize'] );
+
+		$this->assertNotNull( $GLOBALS['wp_customize']->get_setting( 'viewport_basis' ) );
+
+	}
+
+	/**
+	 * Test the viewport_basis control is registered
+	 */
+	function test_register_viewport_basis_control() {
+
+		Go\Customizer\register_color_controls( $GLOBALS['wp_customize'] );
+
+		$this->assertNotNull( $GLOBALS['wp_customize']->get_control( 'viewport_basis' ) );
+
+	}
+
+
+	/**
 	 * Test the rename_panels renames the colors panel properly
 	 */
 	function test_rename_panels_colors() {
