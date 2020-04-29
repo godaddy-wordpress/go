1218767af2608a45419f97ca663d20f51a3ccf77
Rich Tabor <hi@richtabor.com>
2020-04-29 12:45:06 -0400

    Use opacity instead of hide the post title
    This way its still editable.

--- .dev/assets/admin/js/block-filters.js
+++ .dev/assets/admin/js/block-filters.js
@@ -2,14 +2,14 @@ wp.domReady( function() {
 	jQuery( '.block-editor' ).append( GoBlockFilters.inlineStyles );
 
 	if ( GoBlockFilters.hidePageTitle ) {
-		jQuery( '.block-editor .editor-post-title__input' ).hide();
+		jQuery( '.block-editor .editor-post-title__input' ).css( { opacity: 0.5 } );
 	}
 
 	jQuery( '#hide-page-title-checkbox' ).on( 'change', function() {
 		if ( jQuery( this ).is( ':checked' ) ) {
-			jQuery( '.block-editor .editor-post-title__input' ).hide();
+			jQuery( '.block-editor .editor-post-title__input' ).css( { opacity: 0.5 } );
 			return;
 		}
-		jQuery( '.block-editor .editor-post-title__input' ).show();
+		jQuery( '.block-editor .editor-post-title__input' ).css( { opacity: 1 } );
 	} );
 } );
