41f14b05e47f8498fbb8c69646a23ec7a5b93f25
Evan Herman <evan.m.herman@gmail.com>
2020-05-01 19:01:52 -0400

    Update tests

--- .dev/tests/php/test-core.php
+++ .dev/tests/php/test-core.php
@@ -17,6 +17,19 @@ class Test_Core extends WP_UnitTestCase {
 	}
 
 	/**
+	 * Test the hooked development_environment.
+	 */
+	function testHookedDevelopmentEnvironment() {
+
+		$this->assertEquals(
+			10,
+			has_action( 'after_setup_theme', 'Go\Core\development_environment' ),
+			'after_setup_theme is not attached to Go\Core\development_environment. It might also have the wrong priority (validated priority: 10)'
+		);
+
+	}
+
+	/**
 	 * Test the hooked i18n.
 	 */
 	function testHookedi18n() {
@@ -169,6 +182,52 @@ class Test_Core extends WP_UnitTestCase {
 			has_action( 'comment_form_defaults', 'Go\Core\comment_form_reply_title' ),
 			'comment_form_defaults is not attached to Go\Core\comment_form_reply_title. It might also have the wrong priority (validated priority: 10)'
 		);
+
+	}
+
+	/**
+	 * Test that development_environment doesn't include the necessary file when it doesn't exist.
+	 */
+	function testDevelopmentEnvironmentDoesntExist() {
+
+		if ( ! getenv( 'ci' ) ) {
+
+			$this->markTestSkipped( 'Skip this test locally.' );
+
+			return;
+
+		}
+
+		$this->assertNull( Go\Core\development_environment() );
+
+	}
+
+	/**
+	 * Test that development_environment doesn't include the necessary file when it doesn't exist.
+	 *
+	 * Note: This file will not exist in the CI build
+	 */
+	function testDevelopmentEnvironmentExists() {
+
+		$file_path = get_template_directory() . '/.dev/assets/development-environment.php';
+
+		if ( getenv( 'ci' ) ) {
+
+			touch( $file_path );
+
+			file_put_contents( $file_path, "<?php define( 'TEST_CORE', true );" );
+
+			Go\Core\development_environment();
+
+			$this->assertTrue( TEST_CORE );
+
+			return;
+
+		}
+
+		Go\Core\development_environment();
+
+		$this->assertTrue( class_exists( 'Go\Development_Environment' ) );
 
 	}
 
