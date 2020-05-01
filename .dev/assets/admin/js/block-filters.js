ceff0bae1eaa4133ec43341c356ec819ef532e79
Evan Herman <evan.m.herman@gmail.com>
2020-05-01 14:48:13 -0400

    Re-add

--- .dev/assets/admin/js/block-filters.js
+++ .dev/assets/admin/js/block-filters.js
@@ -6,6 +6,22 @@ import { withSelect, withDispatch } from '@wordpress/data';
 const { PluginPostStatusInfo } = wp.editPost;
 import { compose, withInstanceId } from '@wordpress/compose';
 
+wp.domReady( function() {
+	jQuery( '.block-editor' ).append( GoBlockFilters.inlineStyles );
+
+	if ( GoBlockFilters.hidePageTitle ) {
+		jQuery( '.block-editor .editor-post-title__input' ).css( { opacity: 0.5 } );
+	}
+
+	jQuery( '#hide-page-title-checkbox' ).on( 'change', function() {
+		if ( jQuery( this ).is( ':checked' ) ) {
+			jQuery( '.block-editor .editor-post-title__input' ).css( { opacity: 0.5 } );
+			return;
+		}
+		jQuery( '.block-editor .editor-post-title__input' ).css( { opacity: 1 } );
+	} );
+} );
+
 /**
  * Determines whether Go Page Title Toggle is enabled/disabled for the current page.
  *
