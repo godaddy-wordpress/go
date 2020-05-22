cbdd921d94d5bcdeaa1af86338419223765797af
JR Tashjian <jrtashjian@gmail.com>
2020-05-22 14:55:17 -0400

    Force Chromium window size

--- .dev/tests/cypress/plugins/index.js
+++ .dev/tests/cypress/plugins/index.js
@@ -4,4 +4,11 @@ const {
 
 module.exports = (on, config) => {
 	addMatchImageSnapshotPlugin(on, config);
+
+	on( 'before:browser:launch', ( browser, launchOptions ) => {
+		if ( browser.family === 'chromium' && browser.name !== 'electron' && browser.isHeadless ) {
+			launchOptions.args.push( '--window-size=1920,1080' );
+			return launchOptions;
+		}
+	} );
 };
