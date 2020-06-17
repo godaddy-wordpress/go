7a7b271f8bd2e5d7288730fce135e46b2ca24a74
Rich Tabor <hi@richtabor.com>
2020-06-17 10:46:30 -0400

    Update core.php

--- includes/core.php
+++ includes/core.php
@@ -169,6 +169,12 @@ function theme_setup() {
 	// Add support for editor styles.
 	add_theme_support( 'editor-styles' );
 
+	// Add support for experimental custom padding.
+	add_theme_support( 'experimental-custom-spacing' );
+
+	// Add support for experimental link colors.
+	add_theme_support( 'experimental-link-color' );
+
 	// Add custom editor font sizes.
 	add_theme_support(
 		'editor-font-sizes',
