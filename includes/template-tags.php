dd79331dda14841840625b7754947b7893fc015f
Brian Wendt <55599135+vilocity-brian@users.noreply.github.com>
2020-12-20 12:50:08 -0700

    Add target=_blank to social media links
    See issue #625

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -751,7 +751,7 @@ function social_icons( $args = array() ) {
 
 			<?php if ( ! empty( $social_icon['url'] ) || is_customize_preview() ) : ?>
 				<li class="<?php echo esc_attr( sprintf( $args['li_class'], $key ) ) . esc_attr( $visibility ); ?>">
-					<a class="social-icons__icon" href="<?php echo esc_url( $social_icon['url'] ); ?>" aria-label="<?php echo esc_attr( $social_icon['label'] ); ?>" rel="noopener noreferrer">
+					<a class="social-icons__icon" href="<?php echo esc_url( $social_icon['url'] ); ?>" aria-label="<?php echo esc_attr( $social_icon['label'] ); ?>" rel="noopener noreferrer" target="_blank">
 						<?php
 						// Including SVGs, not template files.
 						// phpcs:ignore WPThemeReview.CoreFunctionality.FileInclude.FileIncludeFound
