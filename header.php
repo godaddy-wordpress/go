1e745db6f7c9a805527c382dc8a3e1653e45fec1
Rich Tabor <hi@richtabor.com>
2021-05-24 16:34:08 -0400

    Header 7 tweaks

--- header.php
+++ header.php
@@ -44,8 +44,6 @@ $header_flex_class = in_array( get_theme_mod( 'header_variation', \Go\Core\get_d
 
 			<div class="header__inner<?php echo esc_attr( $header_flex_class ); ?> items-center justify-between h-inherit w-full relative">
 
-
-
 				<div class="header__extras">
 					<?php do_action( 'go_header_social_icons' ); ?>
 					<?php Go\search_toggle(); ?>
