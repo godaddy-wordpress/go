f461b79a22eae69fbf047c189ff1d85cc78a3ea0
prsnbrg <mail@paulrosenberg.de>
2021-06-12 13:46:19 +0200

    Add Social Media Icon for Xing

--- .dev/assets/admin/js/customize/preview/footer.js
+++ .dev/assets/admin/js/customize/preview/footer.js
@@ -101,6 +101,16 @@ export default () => {
 		} );
 	} );
 
+	wp.customize('social_icon_xing', function (value) {
+		value.bind(function (to) {
+			if (to) {
+				$('.social-icon-xing').removeClass('display-none');
+			} else {
+				$('.social-icon-xing').addClass('display-none');
+			}
+		});
+	});
+
 	wp.customize( 'social_icon_pinterest', ( value ) => {
 		value.bind( ( to ) => {
 			if ( to ) {
