730f8b102b89b3c6d8220dda343629a3bd490878
Evan Herman <evan.m.herman@gmail.com>
2021-06-25 17:42:26 -0400

    Update dependencies, move from npm to yarn

--- .dev/deploy-scripts/build.sh
+++ .dev/deploy-scripts/build.sh
@@ -10,7 +10,7 @@ set -euo pipefail
 # Information on the build-release command can be found here: https://github.com/10up/theme-scaffold#npm-commands
 # The specific commands run can be found in package.json under scripts -> build-release
 # composer install --no-dev -o
-# npm install
+# yarn install
 # grunt
 npm run build-release
 
