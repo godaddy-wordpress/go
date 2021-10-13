65f6202f23cfc93976cf6e051b1daefcc94e07a5
Paul <mail@paulrosenberg.de>
2021-10-13 22:00:36 +0200

    Add Social Media Icon for Spotify (#679)
    * Add Social Media Icon for Spotify
    
    * Fix codeclimate for Spotify Icon

--- includes/core.php
+++ includes/core.php
@@ -1168,6 +1168,11 @@ function get_available_social_icons() {
 			'icon'        => get_theme_file_path( 'dist/images/social/youtube.svg' ),
 			'placeholder' => 'https://youtube.com/user',
 		),
+		'spotify'   => array(
+			'label'       => esc_html__( 'Spotify', 'go' ),
+			'icon'        => get_theme_file_path( 'dist/images/social/spotify.svg' ),
+			'placeholder' => 'https://open.spotify.com/user/user',
+		),
 		'github'    => array(
 			'label'       => esc_html__( 'GitHub', 'go' ),
 			'icon'        => get_theme_file_path( 'dist/images/social/github.svg' ),
