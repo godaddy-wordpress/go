cc757a7a5816e7e2830f38d25756954ba5cf0ac5
Jonathan Bardo <jbardo@godaddy.com>
2021-11-04 14:35:22 -0400

    Rename scripts to be consistent with CoBlocks

--- .dev/deploy-scripts/build.sh
+++ .dev/deploy-scripts/build.sh
@@ -7,12 +7,12 @@ set -x
 set -euo pipefail
 
 # These commands assume the 10up scaffolds are used. Change to match your build process
-# Information on the build-release command can be found here: https://github.com/10up/theme-scaffold#npm-commands
-# The specific commands run can be found in package.json under scripts -> build-release
+# Information on the build:release command can be found here: https://github.com/10up/theme-scaffold#npm-commands
+# The specific commands run can be found in package.json under scripts -> build:release
 # composer install --no-dev -o
 # yarn install
 # grunt
-npm run build-release
+yarn build:release
 
 # Stop printing commands to screen
 set +x
