0d928a4bba0c4fbc9f67ab400825242e1e9d0115
Jonathan Bardo <jonathanbardo@users.noreply.github.com>
2021-11-04 14:54:54 -0400

    Rename scripts to be consistent with CoBlocks (#723)
    Co-authored-by: Jonathan Bardo <jbardo@godaddy.com>

--- .dev/deploy-scripts/test.sh
+++ .dev/deploy-scripts/test.sh
@@ -7,8 +7,8 @@ set -x
 set -euo pipefail
 
 # These commands assume the 10up scaffolds are used. Change to match your build process
-# The specific commands run can be found in package.json under scripts -> lint-release
-npm run lint-release
+# The specific commands run can be found in package.json under scripts -> lint:release
+yarn lint:release
 
 # Stop printing commands to screen
 set +x
