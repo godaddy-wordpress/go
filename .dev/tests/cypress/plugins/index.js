7f7da27544e759238fede4b69a04d19766497094
Jonathan Bardo <jbardo@godaddy.com>
2021-11-05 16:09:29 -0400

    modify cypress to match viewport

--- .dev/tests/cypress/plugins/index.js
+++ .dev/tests/cypress/plugins/index.js
@@ -6,9 +6,19 @@ module.exports = (on, config) => {
 	addMatchImageSnapshotPlugin(on, config);
 
 	on( 'before:browser:launch', ( browser, launchOptions ) => {
-		if ( browser.family === 'chromium' && browser.name !== 'electron' && browser.isHeadless ) {
-			launchOptions.args.push( '--window-size=1920,1080' );
-			return launchOptions;
+		if ( ! browser.isHeadless ) {
+			return;
 		}
+
+		if ( browser.family === 'chromium' && browser.name !== 'electron' ) {
+			launchOptions.args.push( '--window-size=1920,1080' );
+		}
+
+		if ( browser.name === 'firefox' ) {
+			launchOptions.args.push('--width=1920')
+			launchOptions.args.push('--height=1080')
+		}
+
+		return launchOptions;
 	} );
 };
