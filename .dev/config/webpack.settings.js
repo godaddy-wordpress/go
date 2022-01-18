7b1c3d4ee2557ee40367eab0cd2a7df293d73a65
Evan Herman <evan.m.herman@gmail.com>
2022-01-18 15:28:25 -0500

    Fix build path to images

--- .dev/config/webpack.settings.js
+++ .dev/config/webpack.settings.js
@@ -67,8 +67,8 @@ module.exports = {
 	copyWebpackConfig: {
 		from: '.dev/assets/**/*.{jpg,jpeg,png,gif,svg}',
 		to({ context, absoluteFilename }) {
-			return 'images/' + absoluteFilename.replace( /^.*(\.dev\/assets\/|images\/|shared\/)/g, '' );
-        },
+			return 'images/' + absoluteFilename.replace( /^.*(\.dev\/assets\/)/g, '' ).replace( 'images/', '');
+		},
 	},
 	BrowserSyncConfig: {
 		host: 'localhost',
