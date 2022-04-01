668a1712d11748fa4880ca1e582741b994851942
Evan Herman <evan.m.herman@gmail.com>
2022-04-01 14:18:54 -0400

    Updates

---
+++ .dev/tests/cypress/integration/visual-regression/green-welcoming.spec.js
@@ -0,0 +1,31 @@
+import { captureDocument, screenshotPathFromUrl, unslashit, showCoBlocksAnimatedObjects } from '../../helpers';
+
+describe( 'VR Testing: green - welcoming', () => {
+    let pages = [];
+
+    it( 'Loads frontpage', () => {
+        let url = "http://go.test/?wpnux_template_loader=1&template=green&style=welcoming&language=en_US";
+
+        cy.visit( url );
+        showCoBlocksAnimatedObjects();
+        captureDocument( screenshotPathFromUrl( url ) );
+
+        cy.get( '#header__navigation' ).then( $headerNavigation => {
+            [ ...$headerNavigation.find( '.menu-item a' ) ].forEach( $navLink => {
+                if ( "Home" === $navLink.textContent ) {
+                  return;
+                }
+                pages.push( $navLink.href );
+            } );
+        } );
+    } );
+
+    it( 'Loads additional pages', () => {
+        pages.forEach( page => {
+            cy.visit( page );
+            showCoBlocksAnimatedObjects();
+            captureDocument( screenshotPathFromUrl( page ) );
+        } );
+    } );
+
+} );
