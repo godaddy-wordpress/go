800a5b608fa7b3624a8f2ac46075dd5ac6f689af
Evan Herman <evan.m.herman@gmail.com>
2023-07-21 11:22:25 -0400

    Update bootstrap.php

--- .dev/tests/php/bootstrap.php
+++ .dev/tests/php/bootstrap.php
@@ -1,19 +1,38 @@
 <?php
 
+// Require dependencies.
+require_once dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/vendor/wp-phpunit/wp-phpunit/__loaded.php';
+require_once dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
+
+// If we're running in WP's build directory, ensure that WP knows that, too.
+if ( 'build' === getenv( 'LOCAL_DIR' ) ) {
+	define( 'WP_RUN_CORE_TESTS', true );
+}
+
+// Determine the tests directory (from a WP dev checkout).
+// Try the WP_TESTS_DIR environment variable first.
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
+// Next, try the WP_PHPUNIT composer package if we're inside wp-env.
+if ( ! $_tests_dir && 'tests-mysql' === getenv( 'WORDPRESS_DB_HOST' ) ) {
+	$_tests_dir = getenv( 'WP_PHPUNIT__DIR' );
+}
+
+// See if we're installed inside an existing WP dev instance.
 if ( ! $_tests_dir ) {
-
-	$_tests_dir = '/var/www/html/wordpress-tests-lib';
-
+	$_try_tests_dir = dirname( __FILE__ ) . '/../../../../../tests/phpunit';
+	if ( file_exists( $_try_tests_dir . '/includes/functions.php' ) ) {
+		$_tests_dir = $_try_tests_dir;
+	}
+}
+// Fallback.
+if ( ! $_tests_dir ) {
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
 
 if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
-
-	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL;
-
+	echo "Could not find $_tests_dir/includes/functions.php, have you run .dev/bin/install-wp-tests.sh ?" . PHP_EOL; // WPCS: XSS ok.
 	exit( 1 );
-
 }
 
 // Give access to tests_add_filter() function.
