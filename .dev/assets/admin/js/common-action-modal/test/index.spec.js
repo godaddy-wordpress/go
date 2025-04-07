4083a58e223e1f9dd963c20d935b288844d84322
Evan Herman <evan.m.herman@gmail.com>
2025-04-07 14:42:30 -0400

    Update

--- .dev/assets/admin/js/common-action-modal/test/index.spec.js
+++ .dev/assets/admin/js/common-action-modal/test/index.spec.js
@@ -14,6 +14,7 @@ import '@testing-library/jest-dom/extend-expect';
  * Internal dependencies.
  */
 import mockData from '../../../../../tests/cypress/fixtures/network/go_optout.json';
+import Modal from '../index';
 
 import { enableFetchMocks } from 'jest-fetch-mock';
 enableFetchMocks();
