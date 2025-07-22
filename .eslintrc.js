240b6b64aae82ba9f5b06f967b8e57c5ebf4d8c6
Evan Herman <eherman@godaddy.com>
2025-07-22 14:27:56 -0400

    Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,12 +16,19 @@ module.exports = {
 		'react-dom': true,
 	},
 
-	parser: 'babel-eslint',
+	parser: '@babel/eslint-parser',
+	parserOptions: {
+		babelOptions: {
+			presets: [ '@babel/preset-env' ],
+		},
+		requireConfigFile: false,
+	},
 
 	rules: {
 		'@wordpress/no-global-active-element': 0, // because we are not using React, no need for this
 		'jsdoc/check-tag-names': [ 2, {
 			definedTags: [ 'jest-environment' ],
 		} ],
+		'jsdoc/newline-after-description': 'off', // Rule doesn't exist in current eslint-plugin-jsdoc version
 	},
 };
