15a93e0eac946aacd1d6b7649e3d96f19c1cc96f
Evan Herman <85256006+eherman-godaddy@users.noreply.github.com>
2025-07-22 10:57:25 -0400

    Bump Woo cart template to version 10.0.0 (#932)

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -12,7 +12,7 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 7.9.0
+ * @version 10.0.0
  */
 
 defined( 'ABSPATH' ) || exit;
